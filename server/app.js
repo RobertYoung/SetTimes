41de0125d8332a4af5aa515d9e9910f47ee00d44
Robert Young <apple@Robs-MacBook-Pro.local>
2016-06-04 21:57:09 +0100

    Updated the server to seach artists on spotify

--- server/app.js
+++ server/app.js
@@ -1,20 +1,46 @@
 var express = require('express');
-var proxy = require('express-http-proxy');
+var httpProxy = require('http-proxy');
 var app = express();
 var jwt = require('express-jwt');
+var dotenv = require('dotenv');
+var https = require('https');
 
+// app.use('/', jwtCheck);
+
+// app.use('/search/artist',)
+
+// Load the .env variables
+dotenv.load();
+
+// Add authorization to the endpoints
 var jwtCheck = jwt({
-  secret: new Buffer(process.env.AUTH0_CLIENT_SECRET,, 'base64'),
-  audience: '4zTcIczty7HnK4cbkVM2tSlzYHL9idxZ'
+  secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
+  audience: process.env.AUTH0_CLIENT_ID
 });
 
-app.use('/', jwtCheck);
+// Create a proxy server
+var apiProxy = httpProxy.createProxyServer();
 
-app.use('/proxy', proxy('www.google.com', {
-  forwardPath: function(req, res) {
-    return require('url').parse(req.url).path;
-  }
-}));
+// End points
+app.get("/search/artist", function(req, res, next){
+  var q = req.query.q;
+
+  var request = https.get(`https://api.spotify.com/v1/search?q=${q}&type=artist`, function (response) {
+    var body = ""
+    response.on('data', function(data) {
+      body += data;
+    });
+    response.on('end', function() {
+      res.send(JSON.parse(body));
+    });
+  });
+
+  request.on('error', function(e) {
+    console.log('Problem with request: ' + e.message);
+  });
+
+  request.end();
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -23,3 +49,5 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+module.exports = app;
